eafd508db548c923dee3b7b048272143359894a4
mkdir700 <mkdir700@gmail.com>
2020-05-27 15:14:35 +0800

    增加后台任务

--- api/main.py
+++ api/main.py
@@ -9,9 +9,6 @@ app = FastAPI()
 @app.get('/sign')
 async def sign(*, username: str, password: str, schoolid=None, sckey=None,
                background_tasks: BackgroundTasks):
-    USER_INFO = {}
-    USER_INFO['username'] = username
-    USER_INFO['password'] = password
-    USER_INFO['schoolid'] = schoolid
-    background_tasks.add_task(interface, USER_INFO, sckey)
+    user_info = {'username': username, 'password': password, 'schoolid': schoolid}
+    background_tasks.add_task(interface, user_info, sckey)
     return {'message': '您的请求已收到,签到任务正在排队进行中'}
